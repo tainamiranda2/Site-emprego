9b1d3157cbab29556268fd4cbf1c9caf3dc6a53d
Taina <tainamirandaferreira@gmail.com>
2020-07-16 18:03:14 -0300

    Site para estudante

---
+++ index.js
@@ -0,0 +1,35 @@
+
+const express = require('express');
+//const bodyParser = require('body-parser');
+const app = express();
+//Rotas
+
+//view
+
+app.set('view engine','ejs');
+
+//static
+
+app.use(express.static('public'));
+//bodyParse
+
+//app.use(bodyParser.urlencoded({ extende: false}))
+//app.use(bodyParser.json());
+//rota 
+
+app.listen(3000, () => {
+    console.log("Sevidor rodando de boas")
+   })
+
+app.get("/formularios/cadastro", (req, res) => {
+    res.render('formularios/login')
+})
+
+app.get("/formularios/cadastro", (req, res) => {
+    res.render('formularios/cadastro')
+})
+
+app.get("/", (req, res)=>{
+    res.render("index");
+})
+
